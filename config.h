f27020eede83147fac878e4db0235da53a221fd1
monogy <monogy@163.com>
2021-02-15 10:01:42 +0800

    打font2补丁：用以显示在配置彩色emoji字体后，无法显示中文

--- config.h
+++ config.h
@@ -5,7 +5,14 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Sauce Code Pro NF:pixelsize=22:antialias=true:autohint=true";
+static char *font = "SauceCodePro NF:pixelsize=22:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+"SauceCodePro:pixelsize=22:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
